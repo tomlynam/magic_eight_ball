4e36b3a01c06b2e8fb689086f52064ae91a0cc09
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-12 16:47:53 -0600

    allow answers to reset

--- magic_eight_bonus.rb
+++ magic_eight_bonus.rb
@@ -10,31 +10,37 @@ def question
  		add_answers
  	elsif user_input == "print answers"
  		print_answers
+ 	elsif user_input == "reset answers"
+ 		reset_answers
  	end
 end
 
-# Computer outputs random answer
 
-@answers = ["It is certain", 
-"It is decidedly so", 
-"Without a doubt", 
-"Yes, definitely",
-"You may rely on it",
-"As I see it, yes",
-"Most likely",
-"Outlook good Yes",
-"Signs point to yes",
-"Reply hazy try again",
-"Ask again later",
-"Better not tell you now",
-"Cannot predict now",
-"Concentrate and ask again",
-"Don't count on it",
-"My reply is no",
-"My sources say no",
-"Outlook not so good",
-"Very doubtful",
-]
+# Computer outputs random answer
+def original_answers 
+	@answers = ["It is certain", 
+	"It is decidedly so", 
+	"Without a doubt", 
+	"Yes, definitely",
+	"You may rely on it",
+	"As I see it, yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful",
+	]
+end
+
 
 def add_answers
 	user_input = gets.strip
@@ -46,14 +52,24 @@ def add_answers
 	question
 end
 
+
 def print_answers
 	puts @answers
 	question
 end
 
+
+def reset_answers
+	original_answers
+	question
+end
+
+
 def answer
 	puts "The Magic Eight Ball says: #{@answers.sample}"
 end
+
+original_answers
 
 while true
 	question
